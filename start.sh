31269e2c5d01a601ecf7ab2869adb0ea9c98347f
Timmeke_ <47719114+TimBilliet@users.noreply.github.com>
2022-10-09 11:54:48 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+screen -dmS alt1 bash -c "mono MinecraftClient.exe email password saicopvp.com"
+sleep 2
+screen -dmS alt2 bash -c "mono MinecraftClient.exe email password saicopvp.com"
+sleep 2
+screen -dmS alt3 bash -c "mono MinecraftClient.exe email password saicopvp.com"
+sleep 2
+screen -dmS alt4 bash -c "mono MinecraftClient.exe email password saicopvp.com"
+sleep 2
+screen -dmS alt5 bash -c "mono MinecraftClient.exe email password saicopvp.com"
